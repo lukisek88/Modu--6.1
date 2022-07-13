e5dabd2406f328fd77458f967b1d87ab5edd759c
Lukasz Kudla <lukisek88@gmail.com>
2022-07-13 19:33:12 +0200

    7.1 Decorate with Lambda

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,36 @@
+package com.kodilla.stream;
+
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.Processor;
+
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        Processor processor = new Processor();
+        Executor codeToExecute = () -> System.out.println("This is ann example text.");
+        processor.execute(codeToExecute);
+
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        String bText = poemBeautifier.beautify("jestem wielki", (text -> text.toUpperCase()));
+        System.out.println(bText);
+
+        String bText1 = poemBeautifier.beautify("JESTEM WIELKI ", (text -> text.repeat(4)));
+        System.out.println(bText1);
+
+        String bText2 = poemBeautifier.beautify("JESTEM WIELKI a moze jednak nie", (text -> text.toLowerCase()));
+        System.out.println(bText2);
+
+        String bText3= poemBeautifier.beautify("jestem wielki", (text -> text.replace("jestem","byłem")));
+        System.out.println(bText3);
+
+        String bText4= poemBeautifier.beautify("jestem wielki", (text -> text+" mam: "+text.length()+" znaków" ));
+        System.out.println(bText4);
+
+
+
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemDecorator.java
@@ -0,0 +1,8 @@
+package com.kodilla.stream.beautifier;
+
+public interface PoemDecorator {
+
+    String decorate(String text);
+
+
+}
