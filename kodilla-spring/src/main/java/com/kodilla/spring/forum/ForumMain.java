34fa8856cbc0e3e26317e3b0ae50a86b9a285c5a
Lukasz Kudla <lukisek88@gmail.com>
2022-09-02 18:40:01 +0200

    14.1

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumMain.java
@@ -0,0 +1,11 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ForumMain.class,args);
+    }
+}
