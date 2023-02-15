370f76de7d3317f3ecc1104af7fee99184a90605
Lukasz Kudla <lukisek88@gmail.com>
2023-02-15 20:15:38 +0100

    31.3

--- kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
@@ -6,26 +6,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
-public class CrudAppTestingApp{
-    public static final String XPATH_INPUT = "//html/body/main/section/form/fieldset/input";
-    public static final String XPATH_TEXTAREA = "//html/body/main/section/form/fieldset[2]/textarea";
-    public static final String XPATH_WAIT_FOR = "//select[1]";
-    public static final String XPATH_SELECT = "//div[contains(@class, \"task-container\")]/form/div/fieldset/select[1]";
+import java.util.Random;
 
+public class CrudAppTestingApp {
+    private static final String XPATH_INPUT = "//html/body/main/section/form/fieldset[1]/input";
+    private static final String XPATH_TEXTAREA = "//html/body/main/section/form/fieldset[2]/textarea";
+    private static final String XPATH_WAIT_FOR = "//select[1]";
+    private static final String XPATH_SELECT = "//div[contains(@class, \"tasks-container\")]/form/div/fieldset/select[1]";
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
-        driver.get("https://interceptor2048.github.io/");
+        driver.get("https://lukisek88.github.io/");
 
-        WebElement searchField = driver.findElement(By.xpath(XPATH_INPUT));
-        searchField.sendKeys("New robotic task");
+        WebElement taskNameField = driver.findElement(By.xpath(XPATH_INPUT));
+        taskNameField.sendKeys("New robotic task");
 
-        WebElement textareaField = driver.findElement(By.xpath(XPATH_TEXTAREA));
-        textareaField.sendKeys("The robotic content");
+        WebElement taskContentField = driver.findElement(By.xpath(XPATH_TEXTAREA));
+        taskContentField.sendKeys("The robotic content");
 
-      //  while (!driver.findElement(By.xpath(XPATH_WAIT_FOR).isDisplayed());
+        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR)).isDisplayed());
 
         WebElement selectCombo = driver.findElement(By.xpath(XPATH_SELECT));
         Select selectBoard = new Select(selectCombo);
         selectBoard.selectByIndex(1);
+
+
     }
+
+
 }
